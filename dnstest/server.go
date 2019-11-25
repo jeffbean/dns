1f4ceeda5df264ef9e610bc381375699cee13724
Jeff Bean <bean@uber.com>
2019-11-25 09:25:50 -0800

    dnstest : adding mostly untested dnstest.Server

---
+++ dnstest/server.go
@@ -0,0 +1,82 @@
+package dnstest
+
+import (
+	"fmt"
+	"net"
+	"sync"
+
+	"github.com/miekg/dns"
+)
+
+// A Server is a DNS server listening on a system-chosen port on the
+// local loopback interface, for use in end-to-end DNS tests.
+type Server struct {
+	Addr string
+
+	PacketConn net.PacketConn
+
+	Config *dns.Server
+
+	// wg counts the number of outstanding DNS requests on this server.
+	// Close blocks until all requests are finished.
+	wg sync.WaitGroup
+
+	// client is configured for use with the server.
+	// Its transport is automatically closed when Close is called.
+	client *dns.Client
+}
+
+// NewServer starts and returns a new Server.
+// The caller should call Close when finished, to shut it down.
+func NewServer(handler dns.Handler) *Server {
+	ts := NewUnstartedServer(handler)
+	ts.Start()
+	return ts
+}
+
+// NewUnstartedServer returns a new Server but doesn't start it.
+//
+// After changing its configuration, the caller should call Start
+//
+// The caller should call Close when finished, to shut it down.
+func NewUnstartedServer(handler dns.Handler) *Server {
+	return &Server{
+		PacketConn: newLocalListener(),
+		Config:     &dns.Server{Handler: handler},
+	}
+}
+
+// Start starts a server from NewUnstartedServer.
+func (s *Server) Start() {
+	if s.client == nil {
+		s.client = &dns.Client{}
+	}
+	s.Addr = s.PacketConn.LocalAddr().String()
+	s.goServe()
+}
+
+// Close shuts down the server and blocks until all outstanding
+// requests on this server have completed.
+func (s *Server) Close() {
+	s.PacketConn.Close()
+	s.Config.Shutdown()
+	s.wg.Wait()
+}
+
+func (s *Server) goServe() {
+	s.wg.Add(1)
+	// Activate uses the set listener to serve from
+	s.Config.PacketConn = s.PacketConn
+	go func() {
+		defer s.wg.Done()
+		s.Config.ActivateAndServe()
+	}()
+}
+
+func newLocalListener() net.PacketConn {
+	l, err := net.ListenPacket("udp", "127.0.0.1:0")
+	if err != nil {
+		panic(fmt.Sprintf("dnstest: failed to listen on a port: %v", err))
+	}
+	return l
+}
